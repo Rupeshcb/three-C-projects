326132ac5a3ec84f52efb59b6084e959fa866c16
Rupeshcb <rupeshchakraborty598@gmail.com>
2023-06-24 10:53:56 +0600

    modified codes

--- charaacters.c
+++ charaacters.c
@@ -7,6 +7,7 @@ int main(void)
 {
     string s = "This is your characters counting place!";
     printf("%s\n", s);
+    // get the user input
     string name = get_string("Write or Paste your words/characters:\n ");
     int n = strlen(name);
     printf("Numbers of Characters:%i\n", n);
